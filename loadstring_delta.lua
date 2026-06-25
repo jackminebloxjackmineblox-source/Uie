de37197cf76542cc5b69460a5084a863eb72107e
jackminebloxjackmineblox-source <jackminebloxjackmineblox@gmail.com>
2026-06-25 18:18:05 -0300

    Adicionar versão loadstring para Delta exploit

---
+++ loadstring_delta.lua
@@ -0,0 +1,6 @@
+-- COPIE E COLE NO DELTA EXECUTOR
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jackminebloxjackmineblox-source/Uie/main/README.md"))()
+
+-- OU USE ESTE FORMATO DIRETO NO DELTA:
+-- Cole o seguinte comando no executor Delta:
+-- loadstring(game:HttpGet("sua_url_aqui"))()
